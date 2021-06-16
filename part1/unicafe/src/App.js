f9b6f0f2be4f767f0ff50c3dcb8dd26663883b8f
Hanna Jurmu <39794290+hjurmu@users.noreply.github.com>
2021-06-16 14:22:52 +0300

    Finish ex. 1.8 extract statistics
    - Extract statistics into its own Statistics component
    - Keep application state in App component

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -12,14 +12,29 @@ const Stat = ({ text, value }) => (
     <>{ text } { value } </>
 )
 
+const Statistics = ({good, neutral, bad}) => {
+  const all = good + neutral + bad
+  const average = (good * 1 + neutral * 0 +  bad * -1 ) / all
+  const goodPercentage = `${good / all * 100} %`
+
+  return (
+    <p>
+        <Stat text='good' value={good} /><br/>
+        <Stat text='neutral' value={neutral} /><br/>
+        <Stat text='bad' value={bad} /><br/>
+        <Stat text='all' value={good + neutral + bad} /><br/>
+        <Stat text='average' value={average} /><br/>
+        <Stat text='positive' value={goodPercentage} /> 
+    </p>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const all = good + neutral + bad
-  const average = (good * 1 + neutral * 0 +  bad * -1 ) / all
-  const goodPercentage = good / all * 100
+
 
   const handleGoodClick = () => setGood(good + 1)
 
@@ -36,15 +51,7 @@ const App = () => {
       <Button handleClick = { handleNeutralClick } text='neutral' />
       <Button handleClick = { handleBadClick } text='bad' />
       <Header text='statistics' />
-      <p>
-        <Stat text='good' value={good} /><br/>
-        <Stat text='neutral' value={neutral} /><br/>
-        <Stat text='bad' value={bad} /><br/>
-        <Stat text='all' value={good + neutral + bad} /><br/>
-        <Stat text='average' value={average} /><br/>
-        <Stat text='positive' value={goodPercentage} /> %
-      </p>
-      
+      <Statistics good = { good } neutral = { neutral } bad = { bad } />
     </div>
   )
 }
